83b52380d0c010b3c0fcef21ba276324bd7bf10c
Jonathan Hefner <jonathan@hefner.pro>
2018-07-01 13:57:06 -0500

    Remove default test

--- test/i18n/interpolate_nested_test.rb
+++ test/i18n/interpolate_nested_test.rb
@@ -1,7 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class I18n::InterpolateNested::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, I18n::InterpolateNested
-  end
 end
